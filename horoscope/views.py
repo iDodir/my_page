0978324803e09348849c075853c4d3352a745512
dodir <mokronosovmw@gmail.com>
2022-03-21 16:26:23 +0500

    [upd] leo, scorpio requests

--- horoscope/views.py
+++ horoscope/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def leo(request):
+    return HttpResponse('Знак зодиака - Лев')
+
+
+def scorpio(request):
+    return HttpResponse('Это знак зодиака - Скорпион')
